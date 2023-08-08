6bf2f6e21b890509f9c75e063846e6b3d0f04722
DarkIrata <github@ipmix.de>
2023-08-09 00:27:40 +0200

    Update Reboot.py
    Check for Docker

--- RackPi/Pages/Reboot.py
+++ RackPi/Pages/Reboot.py
@@ -1,19 +1,28 @@
 from Data.Drawer import Drawer
 from Data.Helper import *
 from Pages.PageBase import PageBase
+import os
 
 class Reboot(PageBase):
+    inDocker = False
+    
     def __init__(self, drawer: Drawer):
         PageBase.__init__(self, drawer)
+        self.inDocker = os.environ.get('InsideDocker', False)
 
     def UpdateCanvas(self):
         if not self.CanUpdate(100):
             return
         
         self.drawer.ClearCanvas()
-        self.drawer.WriteOnCanvas(".......Reboot.......", line=0)
-        self.drawer.WriteOnCanvas("    Hold Button     ", line=1)
-        self.drawer.WriteOnCanvas("     To Reboot     ", line=2)
+        if self.inDocker:
+            self.drawer.WriteOnCanvas(".......Reboot.......", line=0)
+            self.drawer.WriteOnCanvas("   not available    ", line=1)
+            self.drawer.WriteOnCanvas("   inside docker    ", line=2)
+        else:
+            self.drawer.WriteOnCanvas(".......Reboot.......", line=0)
+            self.drawer.WriteOnCanvas("    Hold Button     ", line=1)
+            self.drawer.WriteOnCanvas("     To Reboot     ", line=2)
 
     def OnLongPress(self):
         self.drawer.ClearCanvas()
